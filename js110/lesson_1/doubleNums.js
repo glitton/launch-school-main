a365cf406b18a00f0b70be329df6ff9f128cc24e
Generosa Litton <glcodeworks@gmail.com>
2024-07-28 14:20:54 -0500

    mutate array by doubling numbers

---
+++ js110/lesson_1/doubleNums.js
@@ -0,0 +1,13 @@
+function doubleNumbers(numbers) {
+  let counter = 0;
+
+  while (counter < numbers.length) {
+    let currentNum = numbers[counter];
+    numbers[counter] = currentNum * 2;
+    counter++;
+  }
+  return numbers;
+}
+
+let myNumbers = [1, 4, 3, 7, 2, 6];
+console.log(doubleNumbers(myNumbers));
