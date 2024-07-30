d0e42180ab68833a87589c6b58131f3d483984c8
Generosa Litton <glcodeworks@gmail.com>
2024-07-30 16:09:55 -0500

    add ages with loop, forEach, reduce

---
+++ js110/lesson_1/ages.js
@@ -0,0 +1,38 @@
+let ages = {
+  Herman: 32,
+  Lily: 30,
+  Grandpa: 5843,
+  Eddie: 10,
+  Marilyn: 22,
+  Spot: 237,
+};
+
+// function addAges(object) {
+//   let objectValues = Object.values(object);
+//   let sumOfAges = 0;
+//   objectValues.forEach((element) => (sumOfAges += element));
+//   return sumOfAges;
+// }
+// console.log(addAges(ages));
+
+// let objectValues = Object.values(ages);
+// // console.log(objectValues);
+// let sumOfAges = 0;
+// for (let idx = 0; idx < objectValues.length; idx++) {
+//   let currentElement = objectValues[idx];
+//   // console.log(currentElement);
+//   sumOfAges += currentElement;
+// }
+
+// console.log(sumOfAges);
+
+// let totalAges = 0;
+// Object.values(ages).forEach((age) => (totalAges += age));
+// console.log(totalAges);
+
+let totalAges = Object.values(ages).reduce(
+  (sumOfAges, currentValue) => sumOfAges + currentValue,
+  0
+);
+
+console.log(totalAges);
