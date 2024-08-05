59e010bd47245034531525dec04351752f8b2a2c
Generosa Litton <glcodeworks@gmail.com>
2024-08-05 13:17:14 -0500

    add condition of number = 0

--- js110/small-problems/easy4/numberToString.js
+++ js110/small-problems/easy4/numberToString.js
@@ -18,8 +18,14 @@ D - Data Structure
 String, Array
 
 A - Algorithm
-1.  Multiply string by 1.  This coerces the string to a number
-2.  Return result
+The tricky part of the solution is the "math" for getting the numbers digit by digit. 
+1. The solution gets the rightmost digit (a one's digit) by getting the remainder 
+of dividing the number by 10. For instance, 4321 % 10 returns 1. 
+2. The solution then chops off the rightmost digit to get the next number by 
+reassigning number to the result of Math.floor(number / 10). 
+3.  Consequently, the result string is incrementally built by prepending the 
+looked-up value, using the remainder as the index, to the previous value of result. 
+4.  This process repeats until the value of number becomes 0.
 
 Code
 */
@@ -28,16 +34,22 @@ const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function integerToString(number) {
   let result = "";
-  while (number > 0) {
-    let remainder = number % 10;
-    number = Math.floor(number / 10);
-    // console.log(number);
-    result = DIGITS[remainder] + result;
-    console.log(result);
+  if (number === 0) {
+    result = DIGITS[0];
   }
-  console.log(result);
+  while (number > 0) {
+    let remainder = number % 10; //step 1
+    // console.log("remainder", remainder);
+    number = Math.floor(number / 10); //step 2
+    // console.log("number", number);
+    result = DIGITS[remainder] + result;
+    // console.log("result", result);
+  }
+
+  return result;
 }
 
+//Launch School version
 // function integerToString(number) {
 //   let result = "";
 
@@ -51,7 +63,7 @@ function integerToString(number) {
 //   return result;
 // }
 
-integerToString(4321); // "4321"
-// integerToString(0); // "0"
-// integerToString(5000); // "5000"
-// integerToString(1234567890); // "1234567890"
+console.log(integerToString(4321)); // "4321"
+console.log(integerToString(0)); // "0"
+console.log(integerToString(5000)); // "5000"
+console.log(integerToString(1234567890)); // "1234567890"
