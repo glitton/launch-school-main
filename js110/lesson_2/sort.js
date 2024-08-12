9e7a334fbea0b7a7474e286d079c85407d5fd784
Generosa Litton <glcodeworks@gmail.com>
2024-08-12 12:21:25 -0500

    sort words in ascending order

--- js110/lesson_2/sort.js
+++ js110/lesson_2/sort.js
@@ -14,25 +14,27 @@ let resultAsc = [2, 11, 9, 4, 107, 21, 1].sort((a, b) => a - b); //ascending
 let resultDesc = [2, 11, 9, 4, 107, 21, 1].sort((a, b) => b - a); //descending
 // console.log(resultDesc);
 
-function sortWordsByLength(array) {
-  let finalWords = [];
-  let wordsLengthArray = array.map((element) => {
-    return element.length;
-  });
-  let sortedWordsLengthArray = wordsLengthArray.sort((a, b) => a - b);
+// function sortWordsByLength(array) {
+//   let finalWords = [];
+//   let wordsLengthArray = array.map((element) => {
+//     return element.length;
+//   });
+//   let sortedWordsLengthArray = wordsLengthArray.sort((a, b) => a - b);
+//   // console.log(sortedWordsLengthArray);
 
-  for (let idx = 0; idx < array.length; idx++) {
-    let currentWord = array[idx];
-    if (currentWord.length === sortedWordsLengthArray[idx]) {
-      console.log(sortedWordsLengthArray[idx]);
+//   for (let idx = 0; idx < sortedWordsLengthArray.length; idx++) {
+//     let count = 0;
+//     let currentWordLength = array[count].length;
+//     if (sortedWordsLengthArray[idx] === currentWordLength) {
+//       finalWords.push(array[idx]);
+//       count += 1;
+//     } else {
+//       count += 1;
+//     }
+//   }
 
-      finalWords.push(currentWord);
-    }
-  }
-  // console.log(finalWords);
-
-  return finalWords;
-}
+//   return finalWords;
+// }
 
 let words = ["go", "ahead", "and", "jump"];
-sortWordsByLength(words);
+console.log(words.sort((a, b) => a.length - b.length));
