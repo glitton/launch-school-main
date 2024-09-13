908029d117b40c6f032f512165a83c05db3b8a4f
Generosa Litton <glcodeworks@gmail.com>
2024-09-12 19:14:31 -0600

    sorting prob 9

--- js110/lesson_2/practiceProbs.js
+++ js110/lesson_2/practiceProbs.js
@@ -148,4 +148,23 @@ function outputVowels(obj) {
     });
   });
 }
-outputVowels(obj);
+// outputVowels(obj);
+
+/* 9 
+Given the following data structure, return a new array with the same structure, but with the values in each subarray ordered -- alphabetically or numerically as appropriate -- in ascending order.
+*/
+
+let arrayOfArrays = [
+  ["b", "c", "a"],
+  [2, 11, -3],
+  ["blue", "black", "green"],
+];
+
+let sortedArray = arrayOfArrays.map((subArr) => {
+  if (typeof subArr[0] === "string") {
+    return subArr.slice().sort();
+  } else {
+    return subArr.slice().sort((a, b) => a - b);
+  }
+});
+// console.log(sortedArray);
