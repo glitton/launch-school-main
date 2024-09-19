cc2515acf73f76596384334ddcac136a96957469
Generosa Litton <glcodeworks@gmail.com>
2024-09-19 17:34:21 -0500

    staggered caps 2

--- js110/string-text-processing/staggeredCaps.js
+++ js110/string-text-processing/staggeredCaps.js
@@ -72,21 +72,66 @@ function staggeredCase(str) {
 // console.log(staggeredCase("ALL_CAPS")); // "AlL_CaPs"
 // console.log(staggeredCase("ignore 77 the 4444 numbers")); // "IgNoRe 77 ThE 4444 nUmBeRs"
 
+/*
+Problem: Modify the function from the previous exercise so it ignores non-alphabetic characters when determining whether it should uppercase or lowercase each letter. The non-alphabetic characters should still be included in the return value; they just don't count when toggling the desired case.
+
+Examples/Test Cases:
+Input: string
+Output: string with staggered cases
+
+Explicit Rules:
+- Input is a string
+- includes alpha and non-alpha character in the return value
+- non-alpha and spaces shouldn't be considered when deciding the toggle state 
+- First letter is always capitalized
+
+
+Data Structures: String, can be array
+
+Algorithm:
+1. Initialize a variable, needUpperCase, to a boolean value of true, this will be used to toggle the state of each letter.  This is true to begin with because the first letter needs to be true and all letters start as lower case
+2. Transform string to an array using the split method and lower case each of the characters
+3. Map over the array of lower case characters
+  - Check if each character is in the range of a to z 
+  If it is in the range: 
+    - Add another if condition to check if needUpperCase true
+      - If it is, reassign needUpperCase to false
+      - return the character as upper case
+    - else re-assign needUpperCase to true  
+    - return character to lower case
+  If it is not a letter, return the character  
+4. Join the array and return the final string
+*/
+
 function staggeredCase2(str) {
-  let strNoSpaces = str
-    .split("")
-    .filter((char) => char !== " ")
-    .join(" ");
-  console.log(strNoSpaces);
+  let needUpperCase = true;
+  let staggeredCaseOfLetters = str.toLowerCase().split("");
+
+  return staggeredCaseOfLetters
+    .map((char) => {
+      if (char >= "a" && char <= "z") {
+        if (needUpperCase) {
+          needUpperCase = false;
+          return char.toUpperCase();
+        } else {
+          needUpperCase = true;
+          return char.toLowerCase();
+        }
+      } else {
+        return char;
+      }
+    })
+    .join("");
 }
 
-// console.log(
-//   staggeredCase2("I Love Launch School!") === "I lOvE lAuNcH sChOoL!"
-// );
-// console.log(staggeredCase2("ALL CAPS") === "AlL cApS");
-// console.log(
-//   staggeredCase2("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
-// );
+console.log(
+  staggeredCase2("I Love Launch School!") === "I lOvE lAuNcH sChOoL!"
+);
+// console.log(staggeredCase2("I Love Launch School!"));
+console.log(staggeredCase2("ALL CAPS") === "AlL cApS");
+console.log(
+  staggeredCase2("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+);
 
 /*
 1.  
