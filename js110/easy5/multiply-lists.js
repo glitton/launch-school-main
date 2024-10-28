bfb9367465b68d2e3fff455cacf8cd536b6851ff
Generosa Litton <glcodeworks@gmail.com>
2024-10-28 14:14:28 -0500

    removed practice problems

--- js110/easy5/multiply-lists.js
+++
@@ -1,9 +0,0 @@
-function multiplyList(arr1, arr2) {
-  let productArray = [];
-  for (let idx = 0; idx < arr1.length; idx += 1) {
-    productArray.push(arr1[idx] * arr2[idx]);
-  }
-  return productArray;
-}
-
-console.log(multiplyList([3, 5, 7], [9, 10, 11]));
