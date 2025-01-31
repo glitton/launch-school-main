66ed5fab205cc2964a2c9e2a0916cd0b1ab189a0
Generosa Litton <glcodeworks@gmail.com>
2025-01-30 18:58:34 -0800

    added one from code wars

--- js119-interview-problems/practice-problems.js
+++ js119-interview-problems/practice-problems.js
@@ -473,3 +473,24 @@ lowercase version to be the same.
 // console.log(leastCommonChar("Mississippi")); // "m"
 // console.log(leastCommonChar("Happy birthday!")); // ' '
 // console.log(leastCommonChar("aaaaaAAAA")); // 'a'
+
+/* 30. From codewars https://www.codewars.com/kata/5848565e273af816fb000449/train/javascript
+P: Write a function that transforms the input string to a secret message. Here are the rules:
+
+1. message is a string containing separate words
+2. Encrypt using the ff rules:
+  - 1st letter converted to its ASCII code
+  - second letter switched with the last letter
+E:
+encryptThis("Hello") === "72olle"
+encryptThis("good") === "103doo"
+encryptThis("hello world") === "104olle 119drlo"
+
+*/
+
+console.log(encryptThis("A")); // "65");
+console.log(encryptThis("A wise old owl lived in an oak")); //"65 119esi 111dl 111lw 108dvei 105n 97n 111ka"¿
+console.log(encryptThis("The more he saw the less he spoke")); //"84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"
+console.log(encryptThis("The less he spoke the more he heard")); //"84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare"
+console.log(encryptThis("Why can we not all be like that wise old bird")); //"87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri"
+console.log(encryptThis("Thank you Piotr for all your help")); //"84kanh 121uo 80roti 102ro 97ll 121ruo 104ple"
