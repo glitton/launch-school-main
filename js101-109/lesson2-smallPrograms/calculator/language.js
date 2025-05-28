74378fe3c604ca7f27a9273a65666c957c003eee
Generosa Litton <glcodeworks@gmail.com>
2025-05-28 13:35:31 -0500

    calculator

---
+++ js101-109/lesson2-smallPrograms/calculator/language.js
@@ -0,0 +1,21 @@
+const readline = require("readline-sync");
+const MESSAGES = require("./calculator_messages.json");
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+//Add function to choose language, insert into prompt
+let language = "en";
+function chooseLanguage() {
+  prompt(MESSAGES[language]["chooseLanguage"]);
+  let languageChosen = readline.question();
+  if (languageChosen === "s") {
+    language = "es";
+  }
+  return language;
+}
+
+chooseLanguage();
+prompt(MESSAGES[language]["firstNumber"]);
+let number1 = readline.question();
